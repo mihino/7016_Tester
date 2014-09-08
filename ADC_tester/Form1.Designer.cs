ac543c12debaec0ff53bb242849576eb0d5df026
Tomas Mihalik <tomas.mihalik@gmail.com>
2014-09-09 01:05:33 +0200

    Initial commit

---
+++ ADC_tester/Form1.Designer.cs
@@ -0,0 +1,393 @@
+﻿namespace ADC_tester
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button8 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button9 = new System.Windows.Forms.Button();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.button10 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DiscardNull = true;
+            this.serialPort1.ParityReplace = ((byte)(126));
+            this.serialPort1.PortName = "COM9";
+            this.serialPort1.ReadBufferSize = 20;
+            this.serialPort1.ReadTimeout = 200;
+            this.serialPort1.WriteBufferSize = 20;
+            this.serialPort1.WriteTimeout = 200;
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(244, 434);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(276, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 29);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "#012";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.numericUpDown1.Location = new System.Drawing.Point(403, 14);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(49, 26);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            9,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Location = new System.Drawing.Point(276, 12);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(121, 31);
+            this.buttonOpen.TabIndex = 3;
+            this.buttonOpen.Text = "Open serial port";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(404, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Meraj";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(403, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(235, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Nastav meranie na obidva naraz";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(276, 84);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 29);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "$01332";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(403, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(180, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Precitaj nastavenie ADC";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(276, 119);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(121, 29);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "$012";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(403, 157);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(130, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Precitaj Firmware";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(276, 154);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(121, 29);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "$01F";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(403, 192);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(171, 20);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Precitaj Typ prevodnika";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(276, 189);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(121, 29);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "$01M";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.Location = new System.Drawing.Point(403, 227);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(230, 20);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Precitaj Exitation Voltage Value";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(276, 224);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(121, 29);
+            this.button6.TabIndex = 13;
+            this.button6.Text = "$016";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label7.Location = new System.Drawing.Point(403, 262);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(251, 20);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Nastav Exitation Voltage Value 5V";
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(276, 259);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(121, 29);
+            this.button7.TabIndex = 15;
+            this.button7.Text = "$017+05.000";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label8.Location = new System.Drawing.Point(403, 297);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(251, 20);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Nastav Exitation Voltage Value 0V";
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(276, 294);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(121, 29);
+            this.button8.TabIndex = 17;
+            this.button8.Text = "$017+00.000";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label9.Location = new System.Drawing.Point(403, 332);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(231, 20);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "Nastav Exitation Voltage Value:";
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(276, 329);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(121, 29);
+            this.button9.TabIndex = 19;
+            this.button9.Text = "$017+02.000";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.numericUpDown2.Location = new System.Drawing.Point(630, 332);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(49, 26);
+            this.numericUpDown2.TabIndex = 21;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(276, 364);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(121, 29);
+            this.button10.TabIndex = 22;
+            this.button10.Text = "#010";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox2.Location = new System.Drawing.Point(410, 364);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(152, 26);
+            this.textBox2.TabIndex = 23;
+            this.textBox2.Text = "#010";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(780, 458);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "ADC Tester";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.TextBox textBox2;
+
+    }
+}
+
---
+++ ADC_tester/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace ADC_tester
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            /*
+            while (serialPort1.BytesToRead > 0)
+            {
+                int ch = serialPort1.ReadChar();
+                String response = Convert.ToString((char)ch) + " " + Convert.ToString(ch) +"\n";
+                this.textBox1.AppendText(response.Replace("\r", "\n"));
+            }
+            
+            while (serialPort1.BytesToRead > 0)
+            {
+                byte[] data = new byte[serialPort1.BytesToRead];
+                serialPort1.Read(data, 0, data.Length);
+                //String response = serialPort1.ReadExisting();
+                String response = Encoding.UTF8.GetString(data);
+                this.textBox1.AppendText(response.Replace("\r", "\n"));
+                System.Threading.Thread.Sleep(50);
+            }
+             */
+            String response = serialPort1.ReadExisting();
+            this.textBox1.AppendText(response.Replace("\r", "\n"));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {           
+            try
+            {
+                serialPort1.ReadExisting();
+                serialPort1.Write("#012" + "\r");
+               
+            }
+            catch (Exception excep)
+            {
+                MessageBox.Show(excep.Message);
+            }
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            this.buttonOpen.Text = "Open COM " + this.numericUpDown1.Value;
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            this.serialPort1.Close();
+            this.serialPort1.PortName = "COM" + this.numericUpDown1.Value;
+            try
+            {
+                this.serialPort1.Open();
+                this.buttonOpen.ForeColor = System.Drawing.Color.Green;
+            }
+            catch (Exception excep)
+            {
+                this.buttonOpen.ForeColor = System.Drawing.Color.Red;
+                MessageBox.Show(excep.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.Write("#$01332" + "\r");
+            }
+            catch (Exception excep)
+            {
+                MessageBox.Show(excep.Message);
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.Write("$012" + "\r");
+            }
+            catch (Exception excep)
+            {
+                MessageBox.Show(excep.Message);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                serialPort1.Write("$01F" + "\r");
+            }
+            catch (Exception excep)
+            {
+                MessageBox.Show(excep.Message);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                serialPort1.Write("$01M" + "\r");
+            }
+            catch (Exception excep)
+            {
+                MessageBox.Show(excep.Message);
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                serialPort1.Write("$016" + "\r");
+            }
+            catch (Exception excep)
+            {
+                MessageBox.Show(excep.Message);
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                serialPort1.Write("$017+05.000" + "\r");
+            }
+            catch (Exception excep)
+            {
+                MessageBox.Show(excep.Message);
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                serialPort1.Write("$017+00.000" + "\r");
+            }
+            catch (Exception excep)
+            {
+                MessageBox.Show(excep.Message);
+            }
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            button9.Text = "$017+0" + numericUpDown2.Value +".000";
+                   }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+
+                serialPort1.Write("$017+0" + numericUpDown2.Value + ".000" + "\r");
+            }
+            catch (Exception excep)
+            {
+                MessageBox.Show(excep.Message);
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            textBox2.Text = textBox2.Text.Trim();
+            button10.Text = textBox2.Text;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+             try
+            {
+
+                serialPort1.Write(textBox2.Text + "\r");
+            }
+            catch (Exception excep)
+            {
+                MessageBox.Show(excep.Message);
+            }
+        
+        }
+    }
+}
